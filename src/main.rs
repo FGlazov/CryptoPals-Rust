a9a33280bd6616527c3a3cd2773c3971c5a56798
Fedor Glazov <fedorglazov@gmail.com>
2017-11-18 11:19:05 +0100

    Problem 1
    Return slices instead of substring when converting string to its
    consecutive length 2 subsections.

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,31 @@ use std::u8;
 
 fn main() {
     let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let two_strings = string_to_len_2_sub_strings(input);
 
-    let x : Vec<u8> = two_strings.iter()
+    let two_strings = string_to_len_2_slices(input);
+
+    let x: Vec<u8> = two_strings.iter()
         .map(|c| convert_string_to_u8(c))
         .collect();
     println!("{}", base64::encode(&x));
 }
 
-fn string_to_len_2_sub_strings(input : &str) -> Vec<String> {
-    let chars: Vec<char> = input.chars().collect();
-    let mut two_strings: Vec<String> = Vec::new();
+fn string_to_len_2_slices(input: &str) -> Vec<&str> {
+    let mut result: Vec<&str> = Vec::new();
+
     {
-        let mut counter = 0;
-        let mut string_buffer: String = String::new();
-        for character in chars {
-            string_buffer += &character.to_string();
-            counter = counter + 1;
-            if counter % 2 == 0 {
-                two_strings.push(string_buffer);
-                string_buffer = String::new();
-            }
+        let mut rest_of_input = input;
+        while !rest_of_input.is_empty() {
+            let (to_push, next_rest) = rest_of_input.split_at(2);
+            rest_of_input = next_rest;
+            result.push(to_push);
         }
     }
-    two_strings
+
+    result
 }
 
-fn convert_string_to_u8(string : &str) -> u8 {
+fn convert_string_to_u8(string: &str) -> u8 {
     match u8::from_str_radix(string, 16) {
         Ok(v) => return v,
         Err(e) => panic!("Error during convert_string_to_u8: {}", e)
