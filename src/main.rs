4fe5b6a3d014d3b85863a262617d3c287e115801
Fedor Glazov <fedorglazov@gmail.com>
2017-11-18 11:58:02 +0100

    Problem one
    Add unit test in place of main function.

--- src/main.rs
+++ src/main.rs
@@ -43,8 +43,14 @@ fn convert_string_to_u8(string: &str) -> u8 {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-fn main() {
-    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    println!("{}", input.hex_to_base64());
+    #[test]
+    fn test_example_problem_one() {
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+        assert_eq!(expected, input.hex_to_base64());
+    }
 }
