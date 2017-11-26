4e50fd96223118580f8f4a40eced628d60938f4b
Fedor Glazov <fedorglazov@gmail.com>
2017-11-26 13:46:46 +0100

    Problem 5
    Implement repeating key xor cipher.

--- src/byte_util.rs
+++ src/byte_util.rs
@@ -1,5 +1,32 @@
-pub fn xor_all_bytes(key : &u8, bytes : &Vec<u8>) -> Vec<u8> {
+use std;
+
+pub fn xor_all_bytes(key: &u8, bytes: &Vec<u8>) -> Vec<u8> {
     bytes.iter()
         .map(|x| x ^ key)
         .collect()
+}
+
+pub fn repeating_key_xor<'a, I>(key: I, bytes: I) -> Vec<u8>
+    where I: Iterator<Item=&'a u8> + std::clone::Clone
+{
+    bytes.zip(key.cycle())
+        .map(|(byte, key_part)| byte ^ key_part)
+        .collect()
+}
+
+//todo : Do this better somehow (imports for test only)
+#[allow(unused_imports)]
+use string_util;
+use string_util::StringUtil;
+
+#[test]
+fn test_problem_five() {
+    let text = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    let key = "ICE";
+
+    let actual = repeating_key_xor(key.as_bytes().iter(), text.as_bytes().iter());
+    let expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272\
+                          a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f".hex_to_bytes();
+
+    assert_eq!(expected, actual);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,10 @@ mod byte_util;
 mod string_util;
 mod xor_cracker;
 
-use std::io::BufRead;
-use std::io::BufReader;
-use std::fs::File;
-
 fn main() {
-    let f = match File::open("4.txt") {
-        Ok(a) => { a }
-        Err(b) => { panic!(b) }
-    };
-
-    let lines = BufReader::new(f).lines()
-        .map(|x| x.unwrap());
-
-    let decrypted_results= xor_cracker::detect_single_byte_xor_encryption(lines);
-    for result in decrypted_results {
-        println!("Result: {} Rating: {}", result.decoded_text, result.rating);
-    }
+//    let text = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+//    let key = "ICE";
+//
+//    let encrypted = byte_util::repeating_key_xor(key.as_bytes().iter(), text.as_bytes().iter());
+//    println!("{}" ,string_util::bytes_to_hex(&encrypted))
 }
