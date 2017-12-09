61b257bee234da10f08d04087bf47e8f62f539ab
Fedor Glazov <fedorglazov@gmail.com>
2017-12-09 17:52:54 +0100

    Problem 7 Implement AES
    Implement AddRoundKey.

---
+++ src/aes/add_round_key.rs
@@ -0,0 +1,24 @@
+fn add_round_key(state: &mut [u8], words: &[u8]) {
+    for i in 0..state.len() {
+        state[i] = state[i] ^ words[i];
+    }
+}
+
+mod test {
+    use super::add_round_key;
+    use string_util::StringUtil;
+    use byte_util;
+
+    #[test]
+    fn test_shift_rows() {
+        let input = "c81677bc9b7ac93b25027992b0261996".hex_to_bytes();
+        let mut actual_array = byte_util::from_slice(input.as_slice());
+        let round_key = "3caaa3e8a99f9deb50f3af57adf622aa".hex_to_bytes();
+        add_round_key(&mut actual_array, round_key.as_slice());
+
+        let expected = "f4bcd45432e554d075f1d6c51dd03b3c".hex_to_bytes();
+        let expected_array = byte_util::from_slice(expected.as_slice());
+
+        assert_eq!(expected_array, actual_array);
+    }
+}
--- src/aes/mod.rs
+++ src/aes/mod.rs
@@ -1,3 +1,4 @@
 mod sub_bytes;
 mod shift_rows;
 mod mix_columns;
+mod add_round_key;
