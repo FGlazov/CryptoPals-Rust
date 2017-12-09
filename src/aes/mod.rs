20032223dd90e55fa20a0a3bfa0086f5a9ca534b
Fedor Glazov <fedorglazov@gmail.com>
2017-12-09 20:45:15 +0100

    Problem 7 Implement AES
    Implement aes decryption.

--- src/aes/mod.rs
+++ src/aes/mod.rs
@@ -8,6 +8,7 @@ pub fn encrypt_exact(plaintext: &[u8], key: &[u8]) -> [u8; 16] {
     let key_schedule = key_expansion::key_expansion(key);
     let mut state = [0; 16];
     state.clone_from_slice(plaintext);
+
     add_round_key::add_round_key(&mut state, &key_schedule[0..16]);
 
     for round in 1..10 {
@@ -24,6 +25,27 @@ pub fn encrypt_exact(plaintext: &[u8], key: &[u8]) -> [u8; 16] {
     state
 }
 
+pub fn decrypt_exact(ciphertext: &[u8], key: &[u8]) -> [u8; 16] {
+    let key_schedule = key_expansion::key_expansion(key);
+    let mut state = [0; 16];
+    state.clone_from_slice(ciphertext);
+
+    add_round_key::add_round_key(&mut state, &key_schedule[160..176]);
+
+    for round in (1..10).rev() {
+        shift_rows::inv_shift_rows(&mut state);
+        sub_bytes::inv_sub_bytes(&mut state);
+        add_round_key::add_round_key(&mut state, &key_schedule[16 * round..16 * (round + 1)]);
+        mix_columns::inv_mix_columns(&mut state);
+    }
+
+    shift_rows::inv_shift_rows(&mut state);
+    sub_bytes::inv_sub_bytes(&mut state);
+    add_round_key::add_round_key(&mut state, &key_schedule[0..16]);
+
+    state
+}
+
 mod test {
     use string_util::StringUtil;
 
@@ -34,6 +56,16 @@ mod test {
         let actual = super::encrypt_exact(input.as_slice(), key.as_slice()).to_vec();
 
         let expected = "69c4e0d86a7b0430d8cdb78070b4c55a".hex_to_bytes();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_decrypt_exact() {
+        let input = "69c4e0d86a7b0430d8cdb78070b4c55a".hex_to_bytes();
+        let key = "000102030405060708090a0b0c0d0e0f".hex_to_bytes();
+        let actual = super::decrypt_exact(input.as_slice(), key.as_slice()).to_vec();
+
+        let expected = "00112233445566778899aabbccddeeff".hex_to_bytes();
         assert_eq!(expected, actual);
     }
 }
