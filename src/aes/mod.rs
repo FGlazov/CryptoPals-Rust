ab2d29ce7a12170b63b65c4450c01789d189eeeb
Fedor Glazov <fedorglazov@gmail.com>
2018-02-10 20:44:08 +0100

    Problem 10
    Implement CBC mode.

--- src/aes/mod.rs
+++ src/aes/mod.rs
@@ -75,6 +75,57 @@ pub fn decrypt_ecb(ciphertext: Vec<u8>, key: &[u8]) -> Vec<u8> {
     plaintext
 }
 
+
+pub fn encrypt_cbc(plaintext: Vec<u8>, key: &[u8], iv: &[u8]) -> Vec<u8> {
+    let mut padded_plaintext = plaintext.clone();
+    pkcs_7_padding::pad_to_16(&mut padded_plaintext);
+
+    let mut ciphertext = Vec::with_capacity(padded_plaintext.len());
+    ;
+    for i in 0..padded_plaintext.len() / 16 {
+        let last_ciphertext_block = {
+            if i == 0 {
+                iv.to_vec()
+            } else {
+                let slice: &[u8] = &ciphertext[(i - 1) * 16..(i - 1) * 16 + 16];
+                slice.to_vec()
+            }
+        };
+
+        let to_encrypt = xor(&last_ciphertext_block, &padded_plaintext[i * 16..i * 16 + 16]);
+
+        let ciphertext_block = encrypt_exact(&to_encrypt, key);
+        ciphertext.extend(ciphertext_block.iter());
+    }
+
+    ciphertext
+}
+
+pub fn decrypt_cbc(ciphertext: Vec<u8>, key: &[u8], iv: &[u8]) -> Vec<u8> {
+    let mut last_ciphertext_block = &iv[0..16];
+    let mut plaintext = Vec::with_capacity(ciphertext.len());
+    for i in 0..ciphertext.len() / 16 {
+        let pre_plaintext_block = decrypt_exact(&ciphertext[i * 16..i * 16 + 16], key);
+        let plaintext_block = xor(&pre_plaintext_block, &last_ciphertext_block);
+        plaintext.extend(plaintext_block.iter());
+
+        last_ciphertext_block = &ciphertext[i * 16..i * 16 + 16];
+    }
+
+
+    pkcs_7_padding::depad_from_16(&mut plaintext);
+
+    plaintext
+}
+
+fn xor(a: &[u8], b: &[u8]) -> [u8; 16] {
+    let mut result = [0 as u8; 16];
+    for i in 0..16 {
+        result[i] = a[i] ^ b[i];
+    }
+    result
+}
+
 mod test {
     use string_util::StringUtil;
     use std::io::BufRead;
@@ -113,8 +164,17 @@ mod test {
 
         assert_eq!(decrypted.len(), 1);
         assert_eq!(decrypted.get(0), Some(&(0x12 as u8)));
+    }
 
+    #[test]
+    fn test_cbc_mode() {
+        let input = "The quick brown fox jumps over the lazy dog".as_bytes();
+        let key = "NATURAL FIZZLING".as_bytes();
+        let iv = "YELLOW SUBMARINE".as_bytes();
 
+        let ciphertext = super::encrypt_cbc(input.to_vec(), key, iv);
+        let plaintext = super::decrypt_cbc(ciphertext, key, iv);
+        assert_eq!(input.to_vec(), plaintext);
     }
 
     #[test]
@@ -130,6 +190,27 @@ mod test {
 
         let ciphertext = base64::decode(&lines.join("")).unwrap();
         let actual = super::decrypt_ecb(ciphertext, "YELLOW SUBMARINE".as_bytes());
+        println!("Text: {}", String::from_utf8(actual.clone()).unwrap());
+
+        let rating = rating_creator::create_rating(&String::from_utf8(actual).unwrap());
+        assert!(rating > 1000);
+    }
+
+
+    #[test]
+    fn test_problem_ten() {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("test_resources/10.txt");
+        let f = match File::open(d) {
+            Ok(a) => { a }
+            Err(b) => { panic!(b) }
+        };
+        let lines: Vec<String> = BufReader::new(f).lines()
+            .map(|x| x.unwrap()).collect();
+
+        let ciphertext = base64::decode(&lines.join("")).unwrap();
+        let actual = super::decrypt_cbc(ciphertext, "YELLOW SUBMARINE".as_bytes(),
+                                        &[0 as u8; 16]);
         println!("Text: {}", String::from_utf8(actual.clone()).unwrap());
 
         let rating = rating_creator::create_rating(&String::from_utf8(actual).unwrap());
